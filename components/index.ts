90e19cdcdf0589a31a30e594686aee48b713c2de
Arturo Gomez <arturogomezgz04@gmail.com>
2025-10-20 01:30:27 -0600

    Add CreateAccountForm component and update exports in index.ts

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
 export { default as LoginForm } from './LoginForm';
+export { default as CreateAccountForm } from './CreateAccountForm';
 export { default as FooterLogo } from './FooterLogo';
 export { default as HeaderLogo } from './HeaderLogo';
